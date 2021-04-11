172378be7d303af10970d160772fc9f47788b93b
93acy <acyxxx@gmail.com>
2021-04-11 20:16:14 +0800

    Create solution for Team 4 Net Core CA Project

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -7,15 +7,20 @@ using System.Threading.Tasks;
 namespace Team4NetCoreCAProject.Controllers
 {
     public class LoginController : Controller
+
+    /*privately readonly Database db;
+
+    public LoginController(Database db)
     {
-        private readonly AppData appData;
-        public LoginController(AppData appdata)
-        {
-            this.appData= appData;
-        }
-        //declares the appData
+        this.db = db;
+    }*/
+    {
+        
         public IActionResult Index()
         {
+            ViewData["Is_Login"] = "menu_highlight";
+            ViewData["Title"] = "Login";
+            //
             return View();
         }
         // Should link this and store this in an SQL server
@@ -27,16 +32,63 @@ namespace Team4NetCoreCAProject.Controllers
             {
                 ViewData["username"] = username;
                 ViewData["errMsg"] = " no such user or incorrect password";
-
+                ViewData["Is_Login"] = "menu_highlight";
                 return View("Index");
             }
             else
             {
                 user.SessionID = Guid.NewGuid().ToString();
-                Response.Cookies.Append("sessionID", user.SessioId);
+                //UserId = user.Id;
+               
 
-                return RedirectToAction("Index", "Office");
+                Response.Cookies.Append("sessionID", user.SessioId);
+                //wtf is timestamp
+                Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+                //Redirects them to a page we desire? Prolly store
+                //return RedirectToAction("Index", "Office");
+            };
+            // db.Sessions. add(session); 
+            // To be configured in the database
+            // db.SaveChanges();
+
+            //I think this whole part is just guest ID 
+            string currentGuestId = Request.Cookies["guestID"];
+            User currentGuestUser = db.Users.FirstOrDefault(x => x.Id == currentGuestId);
+            // Cart guestCart = Carts.FirstOrDefault(x=> x.UserId == currentGuestUser.Id);
+            
+            // if guest user did have items in the cart
+
+            if (guestCart != null)
+            {
+                //CartDetails
+                List<CartDetails> guestCartDetails = guestCartDetails.CartDetails.ToList();
+
+                //    Cart existingcart = db.Cast.FirstOrDefault(x =>x.UserId == user.Id);
+                {
+                    foreach (CartDetails cd1 in guestCartDetails)
+                    {
+                        if(cd1.ProductId == cd2.ProductId)
+                        {
+                            cd1.Quantity = cd1.Quantity + cd2.Quantity;
+                            // product in cart made while guest isn't logged in is combined into user'existing car
+                            //remove this product/
+                            //db.Remove(cd2)
+                            //dd.SaveChanges()
+                            break;
+                        }
+                    }
+                }    
+            
+            
+            
+            
+            
+            
+            
             }
+
+
+
         }
 
         }
